e0ec996ff494be9dc0c93b5ff2f6f14bdd64dddf
morishitter <morishitter@gmail.com>
2015-09-18 15:24:38 +0900

    first implementation

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-module.exports = function () {
+var fs = require('fs')
+module.exports = function (property) {
+    var list = JSON.parse(fs.readFileSync('list.json', 'utf-8'))
+    var types = Object.keys(list)
+    var ret
+    types.forEach(function (type, i) {
+        if (list[type].indexOf(property) !== -1) {
+            ret = types[i]
+        }
+    })
+
+    return ret
 }
